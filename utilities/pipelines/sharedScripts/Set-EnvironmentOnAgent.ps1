6ef933d06664a24826650b231ec82d92bbb152d4
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2025-01-22 13:03:46 +0200

    adding import-module

--- utilities/pipelines/sharedScripts/Set-EnvironmentOnAgent.ps1
+++ utilities/pipelines/sharedScripts/Set-EnvironmentOnAgent.ps1
@@ -84,6 +84,9 @@ function Install-CustomModule {
                 Write-Error ('Installation of module [{0}] failed' -f $foundModule.Name)
             }
         }
+
+        # Import needed PowerShell modules
+        Import-Module Az.Subscription
     }
 }
 #endregion
